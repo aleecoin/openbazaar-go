828f1c45587ba37ffbcdd937e5abb1e6324a905e
aleecoin <whatever>
2018-01-30 00:30:29 -0800

    Fix target and path in build script

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 
 TARGETS=${1:-windows/386,windows/amd64,darwin/amd64,linux/386,linux/amd64,linux/arm}
 
 export CGO_ENABLED=1
 docker pull karalabe/xgo-latest
 go get github.com/karalabe/xgo
-mkdir dist && cd dist/
-xgo --targets=$TARGETS ../
+mkdir -p dist
+pushd dist/
+xgo -ldflags '--ldflags -s' --targets=$TARGETS ../
 chmod +x *
+popd
